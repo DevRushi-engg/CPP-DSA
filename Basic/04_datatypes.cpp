251e6446b288e6b6dd9b7bc833e2bfcee13b1d67
DevRushi-engg <rgurav984@gmail.com>
2026-07-16 22:11:26 +0530

    cpp datatypes

---
+++ Basic/04_datatypes.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    // Following are the data types:
+
+    // int data types - it is use to store the whole number both positive and negative.
+    int age = 34;
+    int marks = 89;
+
+    // float data types - its used to store the decimal numbers both positive and negative.
+    float pi = 3.14;
+    float mile = 8.9;
+
+    // boolean data types - its used to state the true or false;
+    bool isgood = true;
+    bool flag = false;
+
+}
