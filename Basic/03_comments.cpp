bf778677d2f68f0431c7e5e587a93e84165521ab
DevRushi-engg <rgurav984@gmail.com>
2026-07-16 22:09:55 +0530

    comments

---
+++ Basic/03_comments.cpp
@@ -0,0 +1,6 @@
+// comments are the information about the code for other developers to understand the code.
+// comments are not executed by the compiler.
+// comments are of two types : 1. Single Line Comments 2. Multi Line Comments
+// Single line comments use by : //
+// Multi line comments are use by : /*  */
+ 
