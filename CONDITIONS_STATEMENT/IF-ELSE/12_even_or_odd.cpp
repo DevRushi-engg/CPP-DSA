235303199ec5e57e1bd62ae153cd8c5925150577
DevRushi-engg <rgurav984@gmail.com>
2026-07-19 13:14:43 +0530

    check even or odd

---
+++ CONDITIONS_STATEMENT/IF-ELSE/12_even_or_odd.cpp
@@ -0,0 +1,17 @@
+#include <bits\stdc++.h>
+using namespace std;
+
+int main(){
+
+    int n;
+    cout<<"Enter the number to check even or odd: ";
+    cin>>n;
+
+    if(n%2==0){
+        cout<<n<<" is even number."<<endl;
+
+    }
+    else{
+        cout<<n<<" is odd number."<<endl;
+    }
+}
