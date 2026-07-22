f4b158f1413c5ddc44824bcbf8d6b9b7da527a0a
DevRushi-engg <rgurav984@gmail.com>
2026-07-22 18:07:22 +0530

    print even nuumbers till 'n'

---
+++ 03_LOOPS/01_FOR_LOOPS/My_Practice_Question/03_print_even_number.cpp
@@ -0,0 +1,14 @@
+//Print even number till n numbers
+
+#include <bits\stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"Enter the number: ";
+    cin>>n;
+
+    for(int i = 0; i <= n; i += 2){
+        cout<<i<<endl;
+    }
+}
