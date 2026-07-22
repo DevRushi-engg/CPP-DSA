3412461c1bb538ea63595e2cab1b3043dbf0b4fd
DevRushi-engg <rgurav984@gmail.com>
2026-07-22 17:57:41 +0530

    print hello world n times

---
+++ 03_LOOPS/01_FOR_LOOPS/My_Practice_Question/01_print_hello_n_times.cpp
@@ -0,0 +1,15 @@
+// Print hello world 'n' time. Take 'n' as input from the user
+
+
+#include <bits\stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"Enter the Number times you want to print Hello World:  ";
+    cin>>n;
+
+    for(int i = 0; i <=n; i++){
+        cout<<"Hello World"<<endl;
+    }
+}
