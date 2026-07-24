7027b2cdf9275f38c874aff8a45d0c9410f46eb7
DevRushi-engg <rgurav984@gmail.com>
2026-07-24 21:54:55 +0530

    print alphabets A to Z

---
+++ 03_LOOPS/01_FOR_LOOPS/01_BASIC_LOOP/10_print_alphabets.cpp
@@ -0,0 +1,12 @@
+// Print the alphabet from A to Z.
+
+#include <bits\stdc++.h>
+using namespace std;
+
+int main(){
+
+    cout<<"Follwing are alphabets from A to Z"<<endl;
+    for(char c = 'A'; c <= 'Z'; c++){
+        cout<<c<< " ";
+    }
+}
