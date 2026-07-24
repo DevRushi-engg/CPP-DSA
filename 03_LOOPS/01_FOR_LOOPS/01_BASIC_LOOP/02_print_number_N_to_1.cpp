fc4a757ee835ccfc44cf225f03f12d27e9de5a29
DevRushi-engg <rgurav984@gmail.com>
2026-07-24 21:17:52 +0530

    print number from N to 1

---
+++ 03_LOOPS/01_FOR_LOOPS/01_BASIC_LOOP/02_print_number_N_to_1.cpp
@@ -0,0 +1,15 @@
+// Print numbers from N to 1.
+
+#include <bits\stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"Enter the number: ";
+    cin>> n;
+
+    for(int i = n; i >= 1; i--){
+
+        cout<<i<<endl;
+    }
+}
