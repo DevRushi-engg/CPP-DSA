a0ed1124b057e7e829c1b5ab142f9ec979bc8013
DevRushi-engg <rgurav984@gmail.com>
2026-07-24 21:46:33 +0530

    count 100 to 1

---
+++ 03_LOOPS/01_FOR_LOOPS/01_BASIC_LOOP/07_count_100_to_1.cpp
@@ -0,0 +1,13 @@
+// Count from 100 to 1.
+
+#include <bits\stdc++.h>
+using namespace std;
+
+int main (){
+    
+    cout<<"Count from 100 to 1"<<endl;
+
+    for(int i = 100; i >= 1; i--){
+        cout<<i<< " ";
+    }
+}
