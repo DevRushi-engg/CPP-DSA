134a312f73b9c0a36cf83eb7eae45c98bb343f7a
DevRushi-engg <rgurav984@gmail.com>
2026-07-25 19:35:08 +0530

    find largest number from 1 to n

---
+++ 03_LOOPS/01_FOR_LOOPS/02_SUM_&_PRODUCT/19_find_largest_number.cpp
@@ -0,0 +1,27 @@
+// Find the largest number from 1 to N (without arrays).
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N;
+    cout << "Enter a positive integer N: ";
+    cin >> N;
+
+    if (N <= 0) {
+        cout << "Please enter a positive integer greater than 0." << endl;
+        return 0;
+    }
+
+    int largest = 1;
+
+    for (int i = 2; i <= N; i++) {
+        if (i > largest) {
+            largest = i; 
+        }
+    }
+
+    cout << "The largest number from 1 to " << N << " is: " << largest << endl;
+
+    return 0;
+}
