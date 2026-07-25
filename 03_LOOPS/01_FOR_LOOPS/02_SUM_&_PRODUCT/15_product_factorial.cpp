fa217e1dca79daabef88496c156d3f3d9d926de7
DevRushi-engg <rgurav984@gmail.com>
2026-07-25 19:31:07 +0530

    factorial of number

---
+++ 03_LOOPS/01_FOR_LOOPS/02_SUM_&_PRODUCT/15_product_factorial.cpp
@@ -0,0 +1,17 @@
+// Product (Factorial) of N.
+
+#include <bits\stdc++.h>
+using namespace std;
+
+int main(){
+
+    int n;
+    cout<<"Enter the number for factorial: ";
+    cin>>n;
+
+    int factorial = 1;
+    for(int i = 1; i <= n; i++){
+        factorial *= i;
+        cout<<"Factorial of "<<i<<"! = "<<factorial<<endl;
+    }
+}
