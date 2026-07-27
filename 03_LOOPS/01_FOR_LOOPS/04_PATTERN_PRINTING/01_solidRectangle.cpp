8e0d2436b7764089e883f649c700bd0c90b47cf4
DevRushi-engg <rgurav984@gmail.com>
2026-07-27 21:28:51 +0530

    print solid rectangle patter

---
+++ 03_LOOPS/01_FOR_LOOPS/04_PATTERN_PRINTING/01_solidRectangle.cpp
@@ -0,0 +1,31 @@
+// Print the pattern
+
+/*
+
+*******
+*******
+*******
+
+*/
+
+#include <bits\stdc++.h>
+using namespace std;
+
+int main(){
+    int row;
+    cout<<"Number of row: ";
+    cin>>row;
+
+    int col;
+    cout<<"Number of col: ";
+    cin>>col;
+
+    cout<<endl; 
+    
+    for(int i = 1; i <= row; i++){
+        for(int j = 1;  j <= col; j++){
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+}
