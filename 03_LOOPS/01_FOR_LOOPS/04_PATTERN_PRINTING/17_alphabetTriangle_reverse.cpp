d63df33f565dea9758cc4ff586897cfaf16343b3
DevRushi-engg <rgurav984@gmail.com>
2026-07-28 21:00:48 +0530

    print alphabet triangle reverse pattern ascii method

---
+++ 03_LOOPS/01_FOR_LOOPS/04_PATTERN_PRINTING/17_alphabetTriangle_reverse.cpp
@@ -0,0 +1,27 @@
+// Print the pattern
+
+/*
+
+A  B  C  D
+A  B  C 
+A  B  
+A  
+
+*/
+
+#include <bits/stdc++.h>
+using  namespace std;
+
+int main(){
+
+    int n;
+    cout<<"Enter the number of rows: ";
+    cin>>n;
+
+    for(int i = 1; i <= n; i++){
+        for(int j = 1; j <= n+1-i; j++){
+            cout<<(char)(j+64)<<"  ";
+        }
+        cout<<endl;
+    }
+}
