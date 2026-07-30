657ed7fbf9c255c9237f1b4f6dfde7a2c5db420a
DevRushi-engg <rgurav984@gmail.com>
2026-07-30 23:49:33 +0530

    star pyramid upside down print

---
+++ 03_LOOPS/01_FOR_LOOPS/04_PATTERN_PRINTING/39_starPyramid_reverse.cpp
@@ -0,0 +1,33 @@
+/*
+
+ * * * * * 
+  * * * * 
+   * * * 
+    * * 
+     * 
+
+*/
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    int n;
+    cout<<"No of rows: ";
+    cin>>n;
+
+    for(int i = 1; i <= n; i++){
+
+        for(int j = 1; j <= i; j++){
+            cout<<" ";
+        }
+        for(int k = 1; k <= n+1-i; k++){
+            cout<<"* ";
+        }
+        cout<<endl;
+
+    }
+
+}
