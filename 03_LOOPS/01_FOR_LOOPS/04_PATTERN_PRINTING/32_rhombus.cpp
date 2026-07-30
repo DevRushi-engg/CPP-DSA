11119b8fed305fd9dac3b8c36efd825db6d1ac98
DevRushi-engg <rgurav984@gmail.com>
2026-07-30 23:47:17 +0530

    print rhombus pattern

---
+++ 03_LOOPS/01_FOR_LOOPS/04_PATTERN_PRINTING/32_rhombus.cpp
@@ -0,0 +1,34 @@
+
+/*
+
+      * * * * *
+     * * * * *
+    * * * * *
+   * * * * *
+  * * * * *
+
+
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    int n;
+    cout<<"no of rows: ";
+    cin>>n;
+
+
+    for(int i = 1; i <= n; i++){
+
+        for(int k = 1; k <= n+1-i; k++){
+            cout<<" ";
+        }
+        for(int j = 1; j <=n; j++){
+            cout<<" *";
+        }
+        cout<<endl;
+    }
+
+}
