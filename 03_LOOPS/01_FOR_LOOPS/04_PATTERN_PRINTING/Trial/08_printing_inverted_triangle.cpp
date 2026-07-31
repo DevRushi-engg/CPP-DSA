f25e142ff3baa1e33c79628b8ff006a81ee9b894
DevRushi-engg <rgurav984@gmail.com>
2026-07-31 21:10:41 +0530

    printing pattern

---
+++ 03_LOOPS/01_FOR_LOOPS/04_PATTERN_PRINTING/Trial/08_printing_inverted_triangle.cpp
@@ -0,0 +1,23 @@
+//printing inverted triangle
+
+/*
+
+ * * * * *
+ * * * *
+ * * * 
+ * *
+ * 
+
+*/
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    for(int row = 1; row <= 5; row++){
+        for(int col = 1; col <= 6-row; col++){
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+}
