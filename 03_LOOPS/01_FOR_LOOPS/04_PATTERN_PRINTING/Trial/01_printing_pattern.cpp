1cbe1be84aa5ba244c46a47ef75600c97c68d7b0
DevRushi-engg <rgurav984@gmail.com>
2026-07-31 21:08:54 +0530

    printing pattern

---
+++ 03_LOOPS/01_FOR_LOOPS/04_PATTERN_PRINTING/Trial/01_printing_pattern.cpp
@@ -0,0 +1,20 @@
+//Print pattern
+/*
+
+* * * * * 
+* * * * * 
+
+*/
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    
+    for(int row = 1; row <= 2; row++){
+        for(int col = 1; col <= 5; col++){
+            cout<<"* ";
+        }
+        cout<< endl;
+    }
+}
