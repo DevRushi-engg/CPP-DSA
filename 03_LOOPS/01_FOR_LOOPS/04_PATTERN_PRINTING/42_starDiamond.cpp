a53a1f5edc6cd15ed16fd9ef8fa9941a19a9b3f9
DevRushi-engg <rgurav984@gmail.com>
2026-07-31 21:08:27 +0530

    star diamond pattern printing

---
+++ 03_LOOPS/01_FOR_LOOPS/04_PATTERN_PRINTING/42_starDiamond.cpp
@@ -0,0 +1,44 @@
+/*
+   *
+  ***
+ *****
+*******
+ *****
+  ***
+   *
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    int n;
+    cout<<"No. of rows: ";
+    cin>>n;
+    int nst = 1;
+    int nsp = n - 1;
+    for(int i = 1 ; i <= 2*n-1; i++){
+        
+        for(int k = 1; k <= nsp; k++){
+            cout<<" ";
+        }
+        if(i <= n - 1){
+            nsp--;
+        }
+        else{
+            nsp++;
+        }
+
+        for(int j =1; j <= nst; j++){
+            cout<<"*";
+        }
+        if(i <= n-1){
+            nst += 2;
+        }
+        else{
+            nst -= 2;
+        }
+        cout<<endl;
+    }
+}
