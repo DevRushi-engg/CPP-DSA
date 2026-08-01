0df4fcfc605425f43e66b09ce283c49fca4aef0c
DevRushi-engg <rgurav984@gmail.com>
2026-08-01 21:01:28 +0530

    practice pattern

---
+++ 03_LOOPS/01_FOR_LOOPS/04_PATTERN_PRINTING/51_APQ.cpp
@@ -0,0 +1,44 @@
+
+/*
+
+  *
+    * *
+   *   *
+  *     *
+ *       *
+*         *
+
+*/
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    for (int i = 0; i < n; ++i)
+        cout << " ";
+
+    cout << "*" << endl;
+
+    for (int i = 1; i <= n; ++i) {
+
+        for (int j = 0; j < n - i; ++j) {
+            cout << " ";
+        }
+
+        cout << "*";
+
+        int m = 2 * i - 1;
+
+        for (int j = 0; j < m; ++j) {
+            cout << " ";
+        }
+
+        cout << "*" << endl;
+    }
+
+    return 0;
+}
