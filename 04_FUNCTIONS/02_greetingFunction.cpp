ffa73ca051ebf6340c5d23230418b136be415ec1
DevRushi-engg <rgurav984@gmail.com>
2026-08-02 23:10:59 +0530

    greeting function

---
+++ 04_FUNCTIONS/02_greetingFunction.cpp
@@ -0,0 +1,12 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void greeting(){
+    cout<<"Hello Rushikesh!!"<<endl;
+    cout<<"Welcome to C++ Programming Daily."<<endl;
+}
+
+int main(){
+    greeting();
+}
