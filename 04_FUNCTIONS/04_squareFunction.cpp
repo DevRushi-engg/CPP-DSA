65f12a32cee3151dbc640ebcd0abf93d75ef5bfe
DevRushi-engg <rgurav984@gmail.com>
2026-08-02 23:11:32 +0530

    square function

---
+++ 04_FUNCTIONS/04_squareFunction.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int square(int x){
+    return x*x;
+}
+int main(){
+    cout<<"------------------"<<endl;
+    cout<<"      SQUARE      "<<endl;
+    cout<<"------------------"<<endl;
+
+    int a;
+    cout<<"Enter the Number: ";
+    cin>> a;
+
+    cout<<"The Sqaure of the number is: "<<square(a)<<endl;
+
+}
