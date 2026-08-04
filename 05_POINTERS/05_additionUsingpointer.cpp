5396351284cb6bfd2b4b2764102fd379eeac367b
DevRushi-engg <rgurav984@gmail.com>
2026-08-04 20:34:50 +0530

    addition of using pointer

---
+++ 05_POINTERS/05_additionUsingpointer.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    int x = 90;
+    int y =10;
+
+    int *p1 = &x;
+    int *p2 = &y;
+
+    cout<<"Addition = "<<*p1 + *p2<<endl;
+}
