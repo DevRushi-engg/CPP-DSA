393e0f771b3556d854bf6b0594d23c101cb42d4a
DevRushi-engg <rgurav984@gmail.com>
2026-08-05 18:27:09 +0530

    null pointer

---
+++ 05_POINTERS/12_nullPointer.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    // Null Pointer is an reserves addressed which we can use later.
+
+    int* p1 = NULL;
+    int *p2 = 0;
+    int *p3 = '\0';
+
+    cout<<"Address of null pointer P1 : "<<p1<<endl;
+    cout<<"Address of null pointer P2 : "<<p2<<endl;
+    cout<<"Address of null pointer P3 : "<<p3<<endl;
+}
