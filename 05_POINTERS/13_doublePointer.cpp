211af4ef89a7e05c9bcc7970fa1814053ecd9de6
DevRushi-engg <rgurav984@gmail.com>
2026-08-05 18:27:22 +0530

    double pointer

---
+++ 05_POINTERS/13_doublePointer.cpp
@@ -0,0 +1,14 @@
+// Double pointer is used to store the address of the pointer.
+
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int x = 9;
+
+    int *ptr = &x;
+
+    int **pt = &ptr;
+
+}
