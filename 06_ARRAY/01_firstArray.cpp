5d11f35b965dd0af27456225cc2e27ee754f441c
DevRushi-engg <rgurav984@gmail.com>
2026-08-06 17:51:29 +0530

    first array program

---
+++ 06_ARRAY/01_firstArray.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    // define array
+    // int arr[5];
+
+    // initialize the array
+    int arr[5] = {1,3,8,9,7};
+
+    // Printing the array
+    for(int i = 0; i < 5; i++){
+        cout<<arr[i]<<" ";
+    }
+}
