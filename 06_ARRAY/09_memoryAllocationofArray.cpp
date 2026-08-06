367ce4eed666683a30a7719667819634c2ccaa0f
DevRushi-engg <rgurav984@gmail.com>
2026-08-06 21:35:05 +0530

    find the address of array elements

---
+++ 06_ARRAY/09_memoryAllocationofArray.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    
+    int arr[]= {2,5,6,98};
+
+    cout<<&arr[0]<<endl;   // 0x61fefc
+    cout<<&arr[1]<<endl;  // 0x61ff00
+    cout<<&arr[3]<<endl;  // 0x61ff08
+    cout<<&arr[4]<<endl;  // 0x61ff0c
+
+    // &arr and arr both provide the address. the address is a starting point arrary
+    cout<<&arr<<endl;// 0x61fefc
+    cout<<arr<<endl;// 0x61fefc
+
+}
