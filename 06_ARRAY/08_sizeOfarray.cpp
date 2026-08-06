d6801952eaf14950dd342cf5eb8f7496b667cb43
DevRushi-engg <rgurav984@gmail.com>
2026-08-06 21:01:03 +0530

    finding size of array

---
+++ 06_ARRAY/08_sizeOfarray.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    
+    int arr[]= {2,5,6,98,52,41,75,85,32,69,58,45,21,85};
+
+    int n = sizeof(arr)/sizeof(arr[5]);
+
+    cout<<n;
+
+}
