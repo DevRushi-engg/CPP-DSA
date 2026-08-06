1b7cd3cd2411bb7ccca1ef830d497b52b68559cb
DevRushi-engg <rgurav984@gmail.com>
2026-08-06 21:32:11 +0530

    finding size of array

--- 06_ARRAY/08_sizeOfarray.cpp
+++ 06_ARRAY/08_sizeOfarray.cpp
@@ -3,10 +3,17 @@ using namespace std;
 
 int main(){
     
-    int arr[]= {2,5,6,98,52,41,75,85,32,69,58,45,21,85};
+    int arr[]= {2,5,6,98};
 
     int n = sizeof(arr)/sizeof(arr[5]);
 
-    cout<<n;
+    cout<<&arr[0]<<endl;   // 0x61fefc
+    cout<<&arr[1]<<endl;  // 0x61ff00
+    cout<<&arr[3]<<endl;  // 0x61ff08
+    cout<<&arr[4]<<endl;  // 0x61ff0c
+
+    // &arr and arr both provide the address. the address is a starting point arrary
+    cout<<&arr<<endl;// 0x61fefc
+    cout<<arr<<endl;// 0x61fefc
 
 }
