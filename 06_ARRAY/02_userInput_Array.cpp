7baaeb8cbe2045386fd19fe705f17be45478fd5b
DevRushi-engg <rgurav984@gmail.com>
2026-08-06 18:17:51 +0530

    userinput in array

---
+++ 06_ARRAY/02_userInput_Array.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    // defining array
+    int arr[5];
+
+    // Taking input array elements
+    cout<<"Enter the element of array of size array[5]: ";
+    for(int i = 0 ; i < 5; i++){
+        cin>>arr[i];
+    }
+
+    cout<<"Elements in your array are arrat[5]: ";
+    for(int j = 0; j < 5; j++){
+        cout<<arr[j]<<" ";
+    }
+}
