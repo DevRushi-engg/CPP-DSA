a726e70c80524728a03987860a92511b37eda9a5
DevRushi-engg <rgurav984@gmail.com>
2026-08-07 22:43:50 +0530

    product of element in array

---
+++ 06_ARRAY/12_porductOfelements.cpp
@@ -0,0 +1,27 @@
+// Calculate the sum of all elements in the given array
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    int n;
+    cout<<"Enter the size of array: ";
+    cin>>n;
+    
+    int arr[n];
+    cout<<"Enter the elements in the array["<<n<<"] = ";
+    // input operation
+    for(int i = 0; i <= n-1; i++){
+        cin>>arr[i];
+    }
+
+    int product = 1;
+    // sum operation
+    for(int i = 0; i <= n-1; i++){
+        product *= arr[i];
+    }
+    cout<<endl;
+    // output
+    cout<<"The sum of element of the array["<<n<<"] = "<<product<<endl;
+}
